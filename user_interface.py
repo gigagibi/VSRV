a374ee36af5eba8fbd9a64dd98b74b7213ca801b
gigagibi <gii01@mail.ru>
2021-11-15 17:20:10 +0300

    added sockets dict for accessing Socket objects

--- user_interface.py
+++ user_interface.py
@@ -1,26 +1,24 @@
 from tkinter import *
 import paho.mqtt.client as mqtt
 
-sockets = ['socket1', 'socket2', 'socket3', 'socket4']
 client = mqtt.Client()
 
 
-def turn_on(socket):
-    client.publish(topic=socket+'/turn', payload='1')
-
-
-def turn_off(socket):
-    client.publish(topic=socket+'/turn', payload='0')
+def turn(socket, mode):
+    if mode=='on':
+        client.publish(topic=socket+'/turn', payload='1')
+    if mode=='off':
+        client.publish(topic=socket+'/turn', payload='0')
 
 
 class Socket:
     def __init__(self, socket):
         self.frame = Frame(root, bg='grey', width=100, height=100)
         self.lbl_voltage = Label(self.frame, text='0.0v', bg='white')
-        self.btn_on = Button(self.frame, text='нажми',
-                             bg='yellow', command=lambda: turn_on(socket))
-        self.btn_off = Button(self.frame, text='нажми',
-                              bg='yellow', command=lambda: turn_off(socket))
+        self.btn_on = Button(self.frame, text='Включить',
+                             bg='yellow', command=lambda: turn(socket, 'on'))
+        self.btn_off = Button(self.frame, text='Выключить',
+                              bg='yellow', command=lambda: turn(socket, 'off'))
         self.lbl_voltage.pack()
         self.btn_on.pack()
         self.btn_off.pack()
@@ -40,20 +38,15 @@ socket_panel_3 = Socket('socket3')
 socket_panel_4 = Socket('socket4')
 
 
+sockets = {'socket1': socket_panel_1, 'socket2': socket_panel_2, 'socket3': socket_panel_3, 'socket4': socket_panel_4}
+
 def on_message(client, userdata, msg):
     topic = msg.topic
     payload = msg.payload.decode("utf-8")
     if '/voltage' in topic:
         socket_number = int(topic[6])
         print(str(socket_number) + ' || ' + payload + 'v')
-        if(socket_number == 1):
-            socket_panel_1.lbl_voltage['text'] = payload+'v'
-        elif(socket_number == 2):
-            socket_panel_2.lbl_voltage['text'] = payload+'v'
-        elif(socket_number == 3):
-            socket_panel_3.lbl_voltage['text'] = payload+'v'
-        elif(socket_number == 4):
-            socket_panel_4.lbl_voltage['text'] = payload+'v'
+        sockets[topic[:7]].lbl_voltage['text'] = payload+'v'
 
 
 def on_connect(client, userdata, flags, rc):
