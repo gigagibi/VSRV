134b1ae4aa14623135cdc4477619e7269fc84ab1
gigagibi <gii01@mail.ru>
2021-11-15 14:05:03 +0300

    added ability to turn on/off sockets and check their voltages

--- socket_mqtt.py
+++ socket_mqtt.py
@@ -6,35 +6,61 @@ client = mqtt.Client()
 
 topics = ['socket1/voltage', 'socket1/turn', 'socket2/voltage',
           'socket2/turn', 'socket3/voltage', 'socket3/turn', 'socket4/voltage', 'socket4/turn']
-sockets_turned = {'socket1' : True, 'socket2' : True, 'socket3' : False, 'socket4' : False}
+sockets_turned = {'socket1': True, 'socket2': True,
+                  'socket3': True, 'socket4': True}
+sockets_voltages = {'socket1': 220,
+                    'socket2': 220, 'socket3': 220, 'socket4': 220}
+
 
 def on_connect(client, userdata, flags, rc):
     client.subscribe([(topics[0], 0), (topics[1], 0), (topics[2], 0), (topics[3], 0),
                      (topics[4], 0), (topics[5], 0), (topics[6], 0), (topics[7], 0)])
 
 
+def send_random_voltage(socket):
+    if sockets_turned[socket] == True:
+        client.publish(topic=socket +
+                       '/voltage', payload=str(random.randint(100, 300)))
+    else:
+        print(tpc + ' OFF')
+
+
+def check_voltage(socket):
+    if sockets_voltages[socket] <= 210 or sockets_voltages[socket] >= 240:
+        sockets_turned[socket] = False
+        client.publish(topic=socket+'/turn', payload='0')
+
+
 def on_message(client, userdata, msg):
-    topic = msg.topic.decode('utf-8')
+    topic = msg.topic
     payload = msg.payload.decode('utf-8')
     print(topic + ' || ' + payload)
 
     if '/turn' in topic and 'socket' in topic:
-        if payload=='1':
-            sockets_turned[int(topic[6])] = True
-        elif payload=='0':
-            sockets_turned[int(topic[6])] = True
-        
+        if payload == '1':
+            print(topic)
+            sockets_turned[topic[:7]] = True
+            print(topic[:7])
+            client.publish(topic=topic[:7]+'/voltage', payload='220')
+        elif payload == '0':
+            print(topic)
+            sockets_turned[topic[:7]] = False
+            print(topic[:7])
+            client.publish(topic=topic[:7]+'/voltage', payload='0')
+
 
 client.connect("localhost", 1883, 45)
 client.loop_start()
-
+client.on_message = on_message
+client.on_connect = on_connect
 try:
     while True:
-        # time.sleep(5)
+        time.sleep(1)
+        # tpc = 'socket3'
         tpc = 'socket'+str(random.randint(1, 4))
-        if sockets_turned[tpc] == True:
-            client.publish(topic=tpc +
-                        '/voltage', payload=str(random.randint(100, 300)))
+        send_random_voltage(tpc)
+        check_voltage(tpc)
+
 except KeyboardInterrupt:
     print('exiting')
     client.disconnect()
--- tkinter_test.py
+++ tkinter_test.py
@@ -1,33 +1,89 @@
 from tkinter import *
 import paho.mqtt.client as mqtt
-from mqtt import Mqtt
-mqtt = Mqtt()
+
+sockets = ['socket1', 'socket2', 'socket3', 'socket4']
+client = mqtt.Client()
 
 
-def on_click():
-    # print('Нажал')
-    title['text'] = 'нажал'
-    mqtt.publish_press()
+def turn_on(socket):
+    client.publish(topic=socket+'/turn', payload='1')
+def turn_off(socket):
+    client.publish(topic=socket+'/turn', payload='0')
     
+
+
 root = Tk()
 
 root['bg'] = 'white'
 root.title('тест')
-root.geometry('400x600')
-
-canvas = Canvas(root, height=200, width=300)
-canvas.pack()
+root.geometry('600x400')
 
 
-frame = Frame(root, bg='grey')
-frame.place(relheight=0.8, relwidth=0.8)
+frame1 = Frame(root, bg='grey', width=100, height=100)
+lbl_voltage_1 = Label(frame1, text='0.0v', bg='white')
+btn_on_1 = Button(frame1, text='нажми', bg='yellow', command=lambda: turn_on('socket1'))
+btn_off_1 = Button(frame1, text='нажми', bg='yellow', command=lambda: turn_off('socket1'))
+lbl_voltage_1.pack()
+btn_on_1.pack()
+btn_off_1.pack()
+frame1.grid(row=0, column=0)
 
-title = Label(frame, text='Текст', bg='red')
-button = Button(frame, text='нажми', bg='yellow', command=on_click)
-title.pack()
-button.pack()
-mqtt.start()
+
+frame2 = Frame(root, bg='grey', width=100, height=100)
+lbl_voltage_2 = Label(frame2, text='0.0v', bg='white')
+btn_on_2 = Button(frame2, text='нажми', bg='yellow', command=lambda: turn_on('socket2'))
+btn_off_2 = Button(frame2, text='нажми', bg='yellow', command=lambda: turn_off('socket2'))
+lbl_voltage_2.pack()
+btn_on_2.pack()
+btn_off_2.pack()
+frame2.grid(row=0, column=1)
+
+
+frame3 = Frame(root, bg='grey', width=100, height=100)
+lbl_voltage_3 = Label(frame3, text='0.0v', bg='white')
+btn_on_3 = Button(frame3, text='нажми', bg='yellow', command=lambda: turn_on('socket3'))
+btn_off_3 = Button(frame3, text='нажми', bg='yellow', command=lambda: turn_off('socket3'))
+lbl_voltage_3.pack()
+btn_on_3.pack()
+btn_off_3.pack()
+frame3.grid(row=0, column=2)
+
+
+frame4 = Frame(root, bg='grey', width=100, height=100)
+lbl_voltage_4 = Label(frame4, text='0.0v', bg='white')
+btn_on_4 = Button(frame4, text='нажми', bg='yellow', command=lambda: turn_on('socket4'))
+btn_off_4 = Button(frame4, text='нажми', bg='yellow', command=lambda: turn_off('socket4'))
+lbl_voltage_4.pack()
+btn_on_4.pack()
+btn_off_4.pack()
+frame4.grid(row=0, column=3)
+
+
+def on_message(client, userdata, msg):
+    topic = msg.topic
+    payload = msg.payload.decode("utf-8")
+    if '/voltage' in topic:
+        socket_number=int(topic[6])
+        print(str(socket_number) + ' || ' + payload + 'v')
+        if(socket_number==1):
+            lbl_voltage_1['text']=payload+'v'
+        elif(socket_number==2):
+            lbl_voltage_2['text']=payload+'v'
+        elif(socket_number==3):
+            lbl_voltage_3['text']=payload+'v'
+        elif(socket_number==4):
+            lbl_voltage_4['text']=payload+'v'
+
+def on_connect(client, userdata, flags, rc):
+    for s in sockets:
+        client.subscribe(s+'/voltage')
+        client.subscribe(s+'/turn')
+
+
+client.on_connect=on_connect
+client.on_message=on_message
+
+
+client.connect("localhost", 1883, 45)
+client.loop_start()
 root.mainloop()
-
-
-
